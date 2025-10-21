33390f9aac0f6a14267a8b413e68703722905897
Alvaro <amardom027@g.educaand.es>
2025-10-21 09:42:33 +0200

    fix practica-bd

--- practica-bd/index.php
+++ practica-bd/index.php
@@ -1,3 +1,41 @@
+<?php
+const NOMBRE = "bd_teoria";
+require("../const_globales/env.php");
+
+function error_page($title, $body) {
+    $html = '<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>' . $title . '</title>
+            </head>
+            <body>
+            ' . $body . '
+            </body>
+            </html>';
+    return $html;
+}
+
+// Realizamos la conexion a la base de datos
+try {
+    @$conexion = mysqli_connect(SERVIDOR, USUARIO, CLAVE, NOMBRE);
+    mysqli_set_charset($conexion, "utf8");
+} catch (Exception $e) {
+    // die("<p>Error, no se ha podido conectar a la base de datos " . $e->getMessage() . "</p></body></html>");
+    die(error_page("Practica BBDD", "<p>Error, no se ha podido conectar a la base de datos " . $e->getMessage()));
+}
+
+// Realizamos la consulta a la base de datos
+try {
+    $consulta = "select cod_asig, denominacion from t_asignatura";
+    $resultado = mysqli_query($conexion, $consulta);
+} catch (Exception $e) {
+    mysqli_close($conexion);
+    // die("<p>Error, no se ha podido realizar la consulta " . $e->getMessage() . "</p></body></html>");
+    die(error_page("Practica BBDD", "<p>Error, no se ha podido realizar la consulta " . $e->getMessage()));
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -26,39 +64,38 @@
 
 <body>
     <?php
-    const NOMBRE = "bd_teoria";
-    require("../const_globales/env.php");
-
-    try {
-        @$conexion = mysqli_connect(SERVIDOR, USUARIO, CLAVE, NOMBRE);
-        mysqli_set_charset($conexion, "utf8");
-    } catch (Exception $e) {
-        die("<p>Error, no se ha podido conectar a la base de datos " . $e->getMessage() . "</p></body></html>");
-    }
-
-    try {
-        $consulta = "select cod_asig, denominacion from t_asignatura";
-        $resultado = mysqli_query($conexion, $consulta);
-    } catch (Exception $e) {
-        mysqli_close($conexion);
-        die("<p>Error, no se ha podido realizar la consulta " . $e->getMessage() . "</p></body></html>");
-    }
+    // Comprobacion que tenemos tuplas
+    if (mysqli_num_rows($resultado) > 0) {
     ?>
-    <h1>Practica BBDD</h1>
-    <form action="index.php" method="post">
-        <label for="asig">Elija la asignatura: </label>
-        <select name="asig" id="asig">
-            <?php while ($tupla = mysqli_fetch_assoc($resultado)) :  ?>
-                <option value="<?= $tupla["cod_asig"] ?>">
-                    <?= htmlspecialchars($tupla["denominacion"]) ?>
-                </option>
-            <?php endwhile; ?>
-        </select>
-        <p>
-            <button type=" submit" name="btnEnviar">Ver notas</button>
-        </p>
-    </form>
+        <h1>Practica BBDD</h1>
+        <form action="index.php" method="post">
+            <label for="asig">Elija la asignatura: </label>
+            <select name="asig" id="asig">
+                <?php while ($tupla = mysqli_fetch_assoc($resultado)) : ?>
+                    <?php
+                    $selected = "";
+                    if (isset($_POST["asig"]) && $_POST["asig"] == $tupla["cod_asig"]) {
+                        $selected = "selected";
+                        $denomi = $tupla["denominacion"];
+                    }
+                    ?>
+                    <option value="<?= $tupla["cod_asig"] ?>" <?= $selected ?>>
+                        <?= htmlspecialchars($tupla["denominacion"]) ?>
+                    </option>
+                    <?php mysqli_free_result($resultado); // Liberar cuando gastemos las tuplas de un select 
+                    ?>
+                <?php endwhile; ?>
+            </select>
+            <p>
+                <button type=" submit" name="btnEnviar">Ver notas</button>
+            </p>
+        </form>
     <?php
+    } else {
+        echo "<h3>La base de datos no tiene aún ninguna asignatura</h3>";
+    }
+    // En este caso tambien se puede preguntar por el $_POST["asig"]
+    // TODO: MOVER ESTO ARRIBA
     if (isset($_POST["btnEnviar"])) {
         try {
             $consulta = "
@@ -69,28 +106,28 @@
                     t_notas
                 JOIN 
                     t_alumnos ON t_notas.cod_alu = t_alumnos.cod_alu
-                JOIN 
-                    t_asignatura ON t_notas.cod_asig = t_asignatura.cod_asig
                 WHERE 
-                    t_asignatura.cod_asig = " . $_POST["asig"] . ";";
+                    t_notas.cod_asig = " . $_POST["asig"] . ";";
             $resultado = mysqli_query($conexion, $consulta);
         } catch (Exception $e) {
             mysqli_close($conexion);
             die("<p>Error, no se ha podido realizar la consulta " . $e->getMessage() . "</p></body></html>");
         }
+        mysqli_close($conexion);
     ?>
-        <h3>Información de las notas</h3>
+        <h3>Información de las notas de <?= $denomi ?></h3>
         <table>
             <tr>
                 <th>Nombre Alumno</th>
                 <th>Nota</th>
             </tr>
-
             <?php while ($tupla = mysqli_fetch_assoc($resultado)) : ?>
                 <tr>
                     <td><?= htmlspecialchars($tupla["nombre"]) ?></td>
                     <td><?= htmlspecialchars($tupla["nota"]) ?></td>
                 </tr>
+                <?php mysqli_free_result($resultado); // Liberar cuando gastemos las tuplas de un select 
+                ?>
             <?php endwhile; ?>
         </table>
     <?php
